f57f82d627a3a8aa6c27feeafd5add6e7b0bdc81
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-10-06 12:22:33 +0530

    resposive word counter created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+  //text entered
+  const [text, setText] = useState('');
 
+  //word count
+  const [wordCount, setWordCount] = useState(0);
+
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    //array of words
+    const words = text.split(' ');
+
+    //updating word count
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== ''){
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+  }, [text]);
+
+  return (
+    <div className = 'container'>
+      <center>
+      <h2>Responsive Paragraph Word Counter</h2>
+      <div>
+        <textarea value = {text} onChange = {changeHandler}></textarea>
+      </div>
+      </center>
+        <div>
+          <p className='word-count'>Word Count: {wordCount}</p>
+        </div>  
+    </div>
+  )
+}
 export default App;
